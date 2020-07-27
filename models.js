a46be75250228e6fd534f593407a019424a21353
Harry231989 <harry23adjei@yahoo.com>
2020-07-27 12:51:19 -0700

    updated file

--- models.js
+++ models.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/myFlixDB' , {useNewUrlParser: true, useUnifiedTopology: true });
 
+const bcrypt = require('bcrypt');
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  favoriteMovies:  [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
 let movieSchema = mongoose.Schema({
    Title: {type: String, required: true},
    Description: {type: String, required: true},
